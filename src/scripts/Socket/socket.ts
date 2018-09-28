4d190c909cd270779d5a2144f4fa068216d5c7f8
jschofie <jschofie@nerdery.com>
2018-09-28 14:06:22 -0500

    Adding disconnection to socket.  Working, but we will do more with this later

--- src/scripts/Socket/socket.ts
+++ src/scripts/Socket/socket.ts
@@ -4,7 +4,20 @@ class Socket {
     private _socket: SocketIOClient.Socket = null;
 
     init() {
-        this._socket = SocketIO('http://10.17.0.180:3000');
+        this._socket = SocketIO('http://10.17.0.180:3000',);
+
+        this._attachEventListners();
+    }
+
+    private _attachEventListners() {
+
+        this._socket.on('handshake', (payload: any) => console.log('handshake: ', payload))
+
+        this._socket.on('connect_error', (error: {}) => {
+            console.log('SocketIO error (probably the server is not running): \n', error);
+            this._socket.close();
+        })
+
     }
 }
 
